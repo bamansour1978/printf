3f9385ad99beee499a75d2d85903bf80ba41d035
bamansour1978 <karatman@gmail.com>
2023-04-27 18:38:00 +0100

    Update memory _help file

--- memory_help.c
+++ memory_help.c
@@ -1,73 +1,73 @@
 #include "main.h"
 
-unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
-void free_buffer(buffer_t *output);
-buffer_t *init_buffer(void);
+unsigned int _memcpy(buff_t *cout, const char *src, unsigned int n);
+void free_buffer(buff_t *cout);
+buff_t *_buffer(void);
 
 /**
  * _memcpy - Copies n bytes from memory area src to
- *           the buffer contained in a buffer_t struct.
- * @output: A buffer_t struct.
+ *           the buffer contained in a buff_t struct.
+ * @cout: A buff_t struct.
  * @src: A pointer to the memory area to copy.
  * @n: The number of bytes to be copied.
  *
  * Return: The number of bytes copied.
  */
-unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
+unsigned int _memcpy(buff_t *cout, const char *src, unsigned int n)
 {
 	unsigned int index;
 
 	for (index = 0; index < n; index++)
 	{
-		*(output->buffer) = *(src + index);
-		(output->len)++;
+		*(cout->buffer) = *(src + index);
+		(cout->len)++;
 
-		if (output->len == 1024)
+		if (cout->len == 1024)
 		{
-			write(1, output->start, output->len);
-			output->buffer = output->start;
-			output->len = 0;
+			write(1, cout->start, cout->len);
+			cout->buffer = cout->start;
+			cout->len = 0;
 		}
 
 		else
-			(output->buffer)++;
+			(cout->buffer)++;
 	}
 
 	return (n);
 }
 
 /**
- * free_buffer - Frees a buffer_t struct.
- * @output: The buffer_t struct to be freed.
+ * free_buffer - Frees a buff_t struct.
+ * @cout: The buff_t struct to be freed.
  */
-void free_buffer(buffer_t *output)
+void free_buffer(buff_t *cout)
 {
-	free(output->start);
-	free(output);
+	free(cout->start);
+	free(cout);
 }
 
 /**
- * init_buffer - Initializes a variable of struct type buffer_t.
+ * _buffer - Initializes a variable of struct type buff_t.
  *
- * Return: A pointer to the initialized buffer_t.
+ * Return: A pointer to the initialized buff_t.
  */
-buffer_t *init_buffer(void)
+buff_t *_buffer(void)
 {
-	buffer_t *output;
+	buff_t *cout;
 
-	output = malloc(sizeof(buffer_t));
-	if (output == NULL)
+	cout = malloc(sizeof(buff_t));
+	if (cout == NULL)
 		return (NULL);
 
-	output->buffer = malloc(sizeof(char) * 1024);
-	if (output->buffer == NULL)
+	cout->buffer = malloc(sizeof(char) * 1024);
+	if (cout->buffer == NULL)
 	{
-		free(output);
+		free(cout);
 		return (NULL);
 	}
 
-	output->start = output->buffer;
-	output->len = 0;
+	cout->start = cout->buffer;
+	cout->len = 0;
 
-	return (output);
+	return (cout);
 }
